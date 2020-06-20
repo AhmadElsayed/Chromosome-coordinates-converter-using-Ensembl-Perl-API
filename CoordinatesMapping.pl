4270296e8176927b2c9975b1250563bc906c15b1
ahmedelsayed12@gmail.com <ahmedelsayed12@gmail.com>
2020-06-21 03:54:57 +0400

    Add files

---
+++ CoordinatesMapping.pl
@@ -0,0 +1,131 @@
+use strict;
+use warnings;
+
+use IO::File;
+use Getopt::Long;
+
+use Bio::EnsEMBL::Registry;
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+use Scalar::Util qw(looks_like_number);
+my $host = 'ensembldb.ensembl.org';
+my $port = 3306;
+my $user = 'anonymous';
+
+$registry->load_registry_from_db( '-host' => $host,
+                                  '-port' => $port,
+                                  '-user' => $user );
+
+my $repeat = 1;
+
+# Start the while loop to process multiple inputs based on user's request
+# After showing the result, the user will be asked whether he wants to enter another input
+# When $repear = 0, the program will exit
+while ($repeat == 1)
+{
+  my $species = "-";
+  my $start = -1;
+  my $end = -1;
+  my $version = "-";
+  my $strand = -999;
+
+  # Get the species from the user
+  while($species eq "-")
+  {
+    print "Enter the species (ex: human) \n";
+    $species = <STDIN>;
+    chomp($species);
+    if($species eq "") { 
+      $species = "-";
+    }
+    else {
+      printf ("%s: %s\n", "Selected Species: ",  $species); 
+    }
+  }
+
+  # Get the assembly version that will be converted to GRCh37
+  while($version eq "-")
+  {
+    print "\nEnter the chromosome assembly version (ex: GRCh38) \n";
+    $version = <STDIN>;
+    chomp($version);
+    if($version eq "") {
+      printf ("%s\n", "Wrong input, Please try again");
+      $version = "-";
+    }
+    else {
+      printf ("%s: %s\n", "Selected version",  $version); 
+    }
+  }
+  
+  # Get the starting coordinate
+  while ($start == -1)
+  {
+    # testing input 1039265
+    print "\nEnter the chromosome coordinates start point (ex: 1039265) \n";
+    my $start_ = <STDIN>;
+  
+    if(looks_like_number($start_))
+    {
+      $start = $start_;
+      printf ("%s: %s\n", "Selected start point",  $start); 
+    }
+    else{
+      printf ("%s\n", "Wrong input, Please try again");
+    }
+  }
+
+  # Get the ending coordinate
+  while ($end == -1)
+  {
+    # testing input 1039365
+    print "\nEnter the chromosome coordinates end point (ex: 1039365) \n";
+    my $end_ = <STDIN>;
+    
+    if(looks_like_number($end))
+    {
+      $end = $end_;
+      printf ("%s: %s\n", "Selected end point",  $end); 
+    }
+    else{
+      printf ("%s\n", "Wrong input, Please try again");
+    }
+  }
+
+  # Get the input strand
+  while ($strand == -999)
+  {
+    # testing input 1039365
+    print "\nEnter the strand (ex: 1) \n";
+    my $strand_ = <STDIN>;
+    
+    if(looks_like_number($strand_))
+    {
+      $strand = $strand_;  
+      printf ("%s: %s\n", "Selected strand",  $strand); 
+    }
+    else{
+      printf ("%s\n", "Wrong input, Please try again");
+    }
+  }
+
+  my $slice_adaptor = $registry-> get_adaptor( $species, 'Core', 'Slice' );
+  
+  # Obtain a slice covering the region from $start to $end (inclusively) of defined chromosome
+  my $old_slice =  $slice_adaptor->fetch_by_region("chromosome", "X", $start, $end, $strand, $version);
+  
+  foreach my $segment ( @{ $old_slice->project('chromosome', 'GRCh37') } ) {
+    my $new = $segment->to_Slice();
+            
+    printf("\n%s ",$segment->to_Slice()->name());  
+  }
+
+  print "\nDo you want to continue (Y / N)";
+  my $IsContinue = <STDIN>;
+  chomp($IsContinue);
+  
+  $repeat = $IsContinue eq 'Y' ? 1 : 0;
+  
+}
+print("\n");
